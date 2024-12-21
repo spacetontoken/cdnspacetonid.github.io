7b8e33f7ceeca9b8e5ee12c427c18307b5f3274a
SPACETON💎 <143197697+spacetontoken@users.noreply.github.com>
2024-12-21 15:00:30 +0300

    Update forms.js

--- forms.js
+++ forms.js
@@ -1,1 +1,9 @@
-document.forms["form"].addEventListener("input", function(e) { var input = this.querySelector("input[name=input]"); var not = input.value.match(/[^a-zA-Z0-9.+,$-]+/g); if (not) { not.forEach(function(text) { input.value = input.value.replace(text, "") }) } })
+document.forms["form"].addEventListener("input", function(e) {
+  var input = this.querySelector("input[name=input]");
+  var not = input.value.match(/[^a-zA-Z0-9_,$-]+/g);
+  if (not) {
+    not.forEach(function(text) {
+      input.value = input.value.replace(text, "")
+    })
+  }
+})
